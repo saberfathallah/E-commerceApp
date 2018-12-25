9301afc0c8c14cd1af99c36f86b52eab4e349754
saber <saberfathallah7@gmail.com>
2018-12-25 12:03:24 +0100

    return user in context

--- graphqlProject/src/server.js
+++ graphqlProject/src/server.js
@@ -2,15 +2,18 @@ import { ApolloServer, gql } from 'apollo-server';
 import schema from './graphql/schema';
 import dotenv from 'dotenv';
 import { verifyToken } from './utils/authorization';
+import getUserById from '../src/graphql/user/services/getUserById';
 
 dotenv.config();
 
 const server = new ApolloServer({
   schema,
-  context:({ req }) => {
+  context: async ({ req }) => {
     const userid = verifyToken(req.headers.authorization);
+      const data = await getUserById(userid);
+    
     return {
-      userid
+      user: data.user ? data.user : null,
     }
   }
  });
