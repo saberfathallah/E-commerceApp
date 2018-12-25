e306c2bb7aff2f59e8dd59438f6a160e356d379a
saber <saberfathallah7@gmail.com>
2018-12-25 12:09:29 +0100

    export functions getUSerById and deleteUSer

--- KoaE-commerceApp/src/handlers/User/index.js
+++ KoaE-commerceApp/src/handlers/User/index.js
@@ -1,3 +1,5 @@
 export { default as login } from './login'; // eslint-disable-line import/prefer-default-export
 export { default as register } from './register';
 export { default as getAllUsers } from './getAllUsers';
+export { default as getUserById } from './getUserById';
+export { default as deleteUser } from './deleteUser';
