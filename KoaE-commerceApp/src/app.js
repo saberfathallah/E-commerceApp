2f5ec62d9a2e1fcf98ff312dd643e614f71d7cff
saber <saberfathallah7@gmail.com>
2018-12-25 12:04:41 +0100

    return routes

--- KoaE-commerceApp/src/app.js
+++ KoaE-commerceApp/src/app.js
@@ -2,7 +2,7 @@ import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import koaJsonError from 'koa-json-error';
 import Route from 'koa-route';
-import userHandlers from './handlers';
+import { userHandlers, categoryHandlers } from './handlers';
 
 const app = new Koa();
 
@@ -15,7 +15,12 @@ app.use(koaJsonError({
 app.use(Route.post('/register', userHandlers.register));
 app.use(Route.post('/login', userHandlers.login));
 app.use(Route.get('/users', userHandlers.getAllUsers));
+app.use(Route.get('/users/:userId', userHandlers.getUserById));
+app.use(Route.delete('/users', userHandlers.deleteUser));
 
+app.use(Route.post('/categories', categoryHandlers.addCategory));
+app.use(Route.get('/categories', categoryHandlers.getAllCategories));
+app.use(Route.get('/categories/:categoryId', categoryHandlers.getCategoryById));
 
 app.on('error', (err) => {
   console.log(`server error ${err}`);
