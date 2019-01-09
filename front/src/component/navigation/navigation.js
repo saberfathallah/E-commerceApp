9ce9a01b3601ef01310e0e46861896238fd0f9fe
saber <saberfathallah7@gmail.com>
2019-01-09 16:58:31 +0100

    add Navigation component

---
+++ front/src/component/navigation/navigation.js
@@ -0,0 +1,51 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'react-apollo';
+import WrapperNavigation from './withNavigationWrapper';
+import { removeCookie } from '../../utils/cookiesStore';
+
+const Navigation = ({
+  className, user, history,
+}) =>
+  (
+    <div className={className}>
+      <ul className="menu">
+        <li><a href="/">Home</a></li>
+        <li><a href="/">Favorites</a></li>
+        <li><a href="/">Panier</a></li>
+        <li><a href="/">Commandes</a></li>
+      </ul>
+      <div className="right-side">
+        {user.firstName ?
+          <div>
+            <span className="navigation-user-name">{user.firstName} {user.lastName}</span>
+            <button
+              onClick={() => {
+                removeCookie('token', { path: '/' });
+                removeCookie('token', { path: '/home' });
+                localStorage.removeItem('token');
+                history.push('/');
+              }}
+              className="logout-btn"
+            >
+              déconnexion
+            </button>
+          </div>
+          :
+          <div style={{ float: 'right' }}>
+            <button onClick={() => history.push('/register')} >register</button>
+            <button onClick={() => history.push('/Login')} >Login</button>
+          </div>}
+      </div>
+    </div>
+  );
+
+Navigation.propTypes = {
+  className: PropTypes.string,
+  history: PropTypes.object,
+  user: PropTypes.object,
+};
+
+export default compose(WrapperNavigation, withRouter)(Navigation);
