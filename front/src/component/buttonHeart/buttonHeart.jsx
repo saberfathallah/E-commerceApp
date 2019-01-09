d3faf387c549e54ef1e607e585eb3e51be9b7745
saber <saberfathallah7@gmail.com>
2019-01-09 16:55:26 +0100

    add ButtonHeart component

---
+++ front/src/component/buttonHeart/buttonHeart.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Button, Icon, Label } from 'semantic-ui-react';
+
+const ButtonExampleLabeledBasic = () => (
+  <div>
+    <Button as="div" labelPosition="right">
+      <Button color="red">
+        <Icon name="heart" />
+        Like
+      </Button>
+      <Label as="a" basic color="red" pointing="left">
+        2,048
+      </Label>
+    </Button>
+  </div>
+);
+
+export default ButtonExampleLabeledBasic;
