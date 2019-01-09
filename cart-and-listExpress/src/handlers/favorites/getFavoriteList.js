d968962b0aaf0bdaa96e74fba306b63204530b92
saber <saberfathallah7@gmail.com>
2019-01-10 00:41:54 +0100

    fix find favoriteList

--- cart-and-listExpress/src/handlers/favorites/getFavoriteList.js
+++ cart-and-listExpress/src/handlers/favorites/getFavoriteList.js
@@ -4,7 +4,7 @@ import validateUserId from '../../utils/validateUserId';
 async function getFavoriteList(req, res) {
   validateUserId(req, res);
   try {
-    const listFavorite = await Favorite.find();
+    const listFavorite = await Favorite.find({ userId: req.headers.userid });
     res.status(200).json(listFavorite);
   } catch (error) {
     res.status(500).json({ error: error.message });
