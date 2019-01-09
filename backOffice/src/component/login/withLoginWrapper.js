8fa5d2435aa1797f0c0155d26fecd8c225f3b304
saber <saberfathallah7@gmail.com>
2019-01-09 16:35:43 +0100

    add CSS for login component

---
+++ backOffice/src/component/login/withLoginWrapper.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export default (Component) => styled(Component)`
+display: flex;
+flex-wrap: wrap; 
+justify-content: center; 
+align-items: center;
+.submit-container {
+  display: flex;
+  justify-content: center; 
+  align-items: center; 
+}
+`;
