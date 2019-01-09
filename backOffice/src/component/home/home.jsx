1b0850b695b759733c72d7427b63764445e535c9
saber <saberfathallah7@gmail.com>
2019-01-09 16:34:56 +0100

    add home component

--- backOffice/src/component/home/home.jsx
+++ backOffice/src/component/home/home.jsx
@@ -1,29 +1,10 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
-import { compose } from 'recompose';
-import { graphql } from 'react-apollo';
-import PropTypes from 'prop-types';
-import { removeCookie } from '../../utils/cookiesStore';
-import ALL_USER_QUERY from '../../graphql/user/getAllUserQuery';
 
-function Home(props) {
+function Home() {
   return (
-    <div>
-      <p>Home</p>
-      <button
-        onClick={() => {
-          removeCookie('token', { path: '/' });
-          props.history.push('/');
-        }}
-      >déconnexion
-      </button>
-    </div>
+    <p>hhhhhhhhhhhhhhhhhhhhh</p>
   );
 }
 
-Home.propTypes = {
-  history: PropTypes.object,
-};
-
-export default compose(graphql(ALL_USER_QUERY), withRouter)(Home);
+export default Home;
 
