9ebf71f96b4b2d895b0d6d1df0de6e1f627d3243
saber <saberfathallah7@gmail.com>
2019-01-09 22:30:14 +0100

    export favorite functions

---
+++ cart-and-listExpress/src/handlers/favorites/index.js
@@ -0,0 +1,3 @@
+export { default as addProductToListFavorite } from './addProductToListFavorite'; // eslint-disable-line import/prefer-default-export
+export { default as getFavoriteList } from './getFavoriteList';
+export { default as deleteFavorite } from './deleteFavorite';
