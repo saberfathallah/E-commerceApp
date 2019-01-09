ed6e7e6f84694d2c07d1d79c53bcc634f2087120
saber <saberfathallah7@gmail.com>
2019-01-09 16:36:50 +0100

    add CSS for tabelCategory component

---
+++ backOffice/src/component/tables/withTabelWrapper.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+export default (Component) => styled(Component)`
+h1{
+  font-size: 30px;
+  color: #fff;
+  text-transform: uppercase;
+  text-align: center;
+  margin-bottom: 15px;
+}
+table{
+  width:100%;
+  table-layout: fixed;
+}
+.tbl-header{
+  background-color: rgba(255,255,255,0.3);
+ }
+.tbl-content{
+  overflow-x:auto;
+  margin-top: 0px;
+  border: 1px solid rgba(255,255,255,0.3);
+}
+th{
+  padding: 20px 15px;
+  text-align: center;
+  font-weight: 500;
+  font-size: 12px;
+  color: #867e7e;
+  text-transform: uppercase;
+}
+td{
+  padding: 15px;
+  text-align: center;
+  vertical-align:middle;
+  font-weight: 300;
+  font-size: 12px;
+  color: #000;
+  border-bottom: solid 1px rgba(255,255,255,0.1);
+}
+
+section{
+  margin: 20px;
+}
+`;
