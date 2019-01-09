bf3fa8bc48c0b38253448be2012b8f7dc09448cb
saber <saberfathallah7@gmail.com>
2019-01-09 17:05:14 +0100

    add delete and update category mutations

--- graphqlProject/src/graphql/category/mutations/categoryMutations.js
+++ graphqlProject/src/graphql/category/mutations/categoryMutations.js
@@ -2,9 +2,13 @@
 import { combineResolvers } from 'graphql-resolvers';
 import addCategory from '../services/addCategory';
 import { isAuthenticated } from '../../../utils/authorization';
+import deleteCategory from '../services/deleteCategory';
+import updateCategory from '../services/updateCategory';
 
 export const categoryMutations = `
   addCategory(input: CategoryInput): categoryResultType
+  updateCategory(input: CategoryInput, id: ID!): categoryResultType
+  deleteCategory(id: ID!): productDeletedType  
 `;
 
 export const Resolvers = {
@@ -12,6 +16,20 @@ export const Resolvers = {
     isAuthenticated,
     async (_, { input }, { user }) => {
       const result = await addCategory(input, user._id);
+      return result;
+    },
+  ),
+  deleteCategory: combineResolvers(
+    isAuthenticated,
+    async (_, { id }, { user }) => {
+      const result = await deleteCategory(id, user._id);
+      return result;
+    },
+  ),
+  updateCategory: combineResolvers(
+    isAuthenticated,
+    async (_, { input, id }, { user }) => {
+      const result = await updateCategory(input, id, user._id);
       return result;
     },
   ),
