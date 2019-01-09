da8647001adbbc3031c63bd63fd2e0bf69750624
saber <saberfathallah7@gmail.com>
2019-01-09 16:31:30 +0100

    add hoc creation form category

---
+++ backOffice/src/component/forms/categoryForm/categoryFormCreation.js
@@ -0,0 +1,48 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import { withFormik } from 'formik';
+import { compose } from 'react-apollo';
+import { withRouter } from 'react-router-dom';
+import withAddCategoryMutation from '../../../graphql/category/addCategory/withAddCategoryMutation';
+import validatorsFunctions, { validateObject } from '../../../utils/validator';
+import CategoryForm from './categoryForm';
+
+function CategoryFormCreation(props) {
+  return <CategoryForm {...props} categoryFormType="creation" />;
+}
+
+function createObjectForValidate() {
+  const validate = Object.assign({}, {
+    level: [validatorsFunctions.isRequire, validatorsFunctions.hasNumber],
+    name: [validatorsFunctions.isRequire],
+    parentId: [validatorsFunctions.isRequire],
+  });
+  return validate;
+}
+
+const formikHoc = withFormik({
+  mapPropsToValues: () =>
+    ({
+      name: '',
+      level: '',
+    }), // initialisation values
+  validate: (values) => { // validation form
+    const firstErrors = validateObject(values, createObjectForValidate());
+    return Object.assign({}, firstErrors);
+  },
+  handleSubmit: async (values, { props, setSubmitting }) => {
+    await props.addCategoryMutation({
+      ...values,
+      level: Number(values.level),
+    });
+    setSubmitting(false);
+  },
+  enableReinitialize: true,
+});
+
+export default
+compose(
+  withAddCategoryMutation,
+  formikHoc,
+  withRouter,
+)(CategoryFormCreation);
