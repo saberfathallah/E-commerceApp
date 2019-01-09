5993f1d9caa2fccb0edf95eabb64050d00acd9b5
saber <saberfathallah7@gmail.com>
2019-01-09 16:24:52 +0100

    export delete and update category function

--- KoaE-commerceApp/src/handlers/Category/index.js
+++ KoaE-commerceApp/src/handlers/Category/index.js
@@ -2,3 +2,5 @@ export { default as addCategory } from './addCategory'; // eslint-disable-line i
 export { default as getAllCategories } from './getAllCategories';
 export { default as getCategoryById } from './getCategoryById';
 export { default as getCategoriesByParentId } from './getCategoriesByParentId';
+export { default as deleteCategory } from './deleteCategory';
+export { default as updateCategory } from './updateCategory';
