00ad885f3d035374d41f8d439c06f368dff8dfe0
saber <saberfathallah7@gmail.com>
2019-01-09 22:28:06 +0100

    export favoriteHandlers

---
+++ cart-and-listExpress/src/handlers/index.js
@@ -0,0 +1,20 @@
+import {
+  addProductToListFavorite,
+  getFavoriteList,
+  deleteFavorite,
+} from './favorites';
+
+const favoriteHandlers = {
+  addProductToListFavorite,
+  getFavoriteList,
+  deleteFavorite,
+};
+
+const aa = {
+  aaa: 'ggg',
+};
+
+export {
+  favoriteHandlers,
+  aa,
+};
