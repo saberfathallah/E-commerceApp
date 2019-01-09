987a9e94b0c49f6bda950e996b0a129357072985
saber <saberfathallah7@gmail.com>
2019-01-09 22:31:50 +0100

    throw error

--- cart-and-listExpress/src/utils/validateSchema.js
+++ cart-and-listExpress/src/utils/validateSchema.js
@@ -3,7 +3,8 @@ import Joi from 'joi';
 export default function validateSchema(res, data, schema) {
   Joi.validate(data, schema, (error) => {
     if (error) {
-      res.status(400).json({ error });
+      res.status(400).json({ error: 'verify params' });
+      throw new Error('verify params');
     }
   });
 }
--- cart-and-listExpress/src/utils/validateUserId.js
+++ cart-and-listExpress/src/utils/validateUserId.js
@@ -1,8 +1,8 @@
 
-export default function validateUserId(res) {
-    const { userid } = res.request.header;
-    if (!userid) {
-      ctx.status(422).json({ error : 'error authentification '});
-    }
+export default function validateUserId(req, res) {
+  const { userid } = req.headers;
+  if (!userid) {
+    res.status(422).json({ error: 'error authentification' });
+    throw new Error('error authentification');
   }
-  
+}
