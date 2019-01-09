c8154d6d7f67550ed5d56c9399df7b0c7d3ac6cc
saber <saberfathallah7@gmail.com>
2019-01-09 22:29:51 +0100

    add getFavoriteList function

---
+++ cart-and-listExpress/src/handlers/favorites/getFavoriteList.js
@@ -0,0 +1,14 @@
+import Favorite from '../../db/models/favorite';
+import validateUserId from '../../utils/validateUserId';
+
+async function getFavoriteList(req, res) {
+  validateUserId(req, res);
+  try {
+    const listFavorite = await Favorite.find();
+    res.status(200).json(listFavorite);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+export default getFavoriteList;
