24f5df325024d1c1b0607ec357296dfaaec7c42a
saber <saberfathallah7@gmail.com>
2019-01-09 17:07:10 +0100

    add productsDetails and categoriesDetails fields

--- graphqlProject/src/graphql/category/types/category.js
+++ graphqlProject/src/graphql/category/types/category.js
@@ -4,8 +4,19 @@ const category = `
     name: String
     parentId: String
     level: Int
-    categories: categoriesResultType
+    productsDetails: productsDetails
+    categoriesDetails: categoriesDetails
+    _id: ID
+  }
+
+  type productsDetails {
     products: productsResultType
+    numberOfProducts: Int,
+  }
+
+  type categoriesDetails {
+    categories: categoriesResultType
+    numberOfCategories: Int
   }
 
   input CategoryInput {
