833fa15a9530dea2925b06c899de56ca4a338e8e
saber <saberfathallah7@gmail.com>
2019-01-09 16:32:08 +0100

    add CSS for form

---
+++ backOffice/src/component/forms/withFormWrapper.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export default (Component) => styled(Component)`
+.form {
+  padding-top: 20px;
+  margin: 0px auto;
+  width: 50%;
+}
+h1 {
+  font-size: 30px;
+  color: #fff;
+  text-transform: uppercase;
+  text-align: center;
+  margin-bottom: 15px;
+}
+.submit-container {
+  text-align: center
+}
+`;
