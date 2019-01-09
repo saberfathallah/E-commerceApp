457785f4a5e7ada8348a34218675cc8e6d34d418
saber <saberfathallah7@gmail.com>
2019-01-09 22:31:25 +0100

    remove pages

--- cart-and-listExpress/src/utils/verifyParams.js
+++
@@ -1,7 +0,0 @@
-import validateSchema from './validateSchema';
-import validateUserId from './validateUserId';
-
-export default function verifyParams(res, data, schema) {
-  validateUserId(res);
-  validateSchema(res, data, schema);
-}
