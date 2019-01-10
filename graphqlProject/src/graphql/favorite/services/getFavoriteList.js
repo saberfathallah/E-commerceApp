edd2ac028ea613a0df586f114db10b4a49e6c719
saber <saberfathallah7@gmail.com>
2019-01-10 13:05:58 +0100

    return product object

--- graphqlProject/src/graphql/favorite/services/getFavoriteList.js
+++ graphqlProject/src/graphql/favorite/services/getFavoriteList.js
@@ -1,4 +1,6 @@
 import fetch from 'node-fetch';
+import { map } from 'lodash';
+import getProductById from '../../product/services/getProductById';
 
 async function getFavoriteListMicroService(userid) {
   const url = `${process.env.CART_AND_LISTS_SERVICES}/favorites`;
@@ -14,7 +16,12 @@ async function getFavoriteListMicroService(userid) {
 async function getFavoriteList(userid) {
   const result = await getFavoriteListMicroService(userid);
   const list = await result.json();
-  return list;
+  // eslint-disable-next-line no-underscore-dangle
+  const items = await Promise.all(map(list, async (item) => {
+    const elm = await getProductById(item.productId);
+    return elm.product;
+  }));
+  return items;
 }
 
 export default getFavoriteList;
