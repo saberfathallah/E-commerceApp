18bfa67e6e6ee2565edccfa58f1707bdf6dc1e6f
saber <saberfathallah7@gmail.com>
2019-01-10 11:16:06 +0100

    create AddOrRemoveFavorite component

--- front/src/component/buttonHeart/buttonHeart.jsx
+++ front/src/component/buttonHeart/buttonHeart.jsx
@@ -1,18 +1,45 @@
 import React from 'react';
-import { Button, Icon, Label } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { compose } from 'react-apollo';
+import { Button, Icon } from 'semantic-ui-react';
+import withAddFavoriteMutation from '../../graphql/favorite/addFavorite/withAddFavoriteMutation';
+import withDeleteFavoriteMutation from '../../graphql/favorite/deleteFavorite/withDeleteFavoriteMutation';
 
-const ButtonExampleLabeledBasic = () => (
-  <div>
-    <Button as="div" labelPosition="right">
-      <Button color="red">
-        <Icon name="heart" />
+function AddOrRemoveFavorite({
+  isFavorite, addFavoriteMutation,
+  deleteFavoriteMutation,
+  user,
+  productId,
+}) {
+  const addorRemove = async () => {
+    if (user.firstName) {
+      if (isFavorite) {
+        await deleteFavoriteMutation(productId);
+      }
+      await addFavoriteMutation(productId);
+    }
+  };
+
+  return (
+    <div>
+      <Button style={{ float: 'right' }} as="div" labelPosition="right">
+        <Button
+          color={isFavorite ? 'green' : 'red'}
+          onClick={() => addorRemove()}
+        >
+          <Icon name="heart" />
         Like
+        </Button>
       </Button>
-      <Label as="a" basic color="red" pointing="left">
-        2,048
-      </Label>
-    </Button>
-  </div>
-);
+    </div>
+  );
+}
+AddOrRemoveFavorite.propTypes = {
+  isFavorite: PropTypes.bool,
+  user: PropTypes.object,
+  addFavoriteMutation: PropTypes.func,
+  deleteFavoriteMutation: PropTypes.func,
+  productId: PropTypes.string,
+};
 
-export default ButtonExampleLabeledBasic;
+export default compose(withAddFavoriteMutation, withDeleteFavoriteMutation)(AddOrRemoveFavorite);
