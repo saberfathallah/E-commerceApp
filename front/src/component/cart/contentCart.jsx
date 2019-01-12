8ecb9bbd3d2c4e9b5f6b06ee3d22e72eaecae4ac
saber <saberfathallah7@gmail.com>
2019-01-12 13:45:38 +0100

    create order function

--- front/src/component/cart/contentCart.jsx
+++ front/src/component/cart/contentCart.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { graphql } from 'react-apollo';
+import { graphql, compose } from 'react-apollo';
 import { get, map } from 'lodash';
+import { withRouter } from 'react-router-dom';
+import { Button } from 'semantic-ui-react';
 import AllItems from '../items';
 import getItemsCurrentCart from '../../graphql/cart/getItemsCurrentCart';
+import withCreateOrderMutation from '../../graphql/order/createOrder/withCreateOrderMutation';
 
 function ContentCart({
-  data, user, active, cart,
+  data, user, active, cart, createOrderMutation, history,
 }) {
   const products = get(data, 'getItemsCurrentCart.items', []);
   const loading = get(data, 'loading', true);
   const numberOfproducts = products.length;
-  const totalCart = get(cart, 'currentCart.cart.total', 0);
+  const total = get(cart, 'currentCart.cart.total', 0);
+  const itemsToOrder = get(cart, 'currentCart.cart.items', []);
+
+  const createOrder = async () => {
+    const items = map(itemsToOrder, (item) => ({
+      quantity: item.quantity,
+      productId: item.productId,
+    }));
+
+    await createOrderMutation({ total, items, adress: user.adress });
+    history.push('/');
+  };
 
   if (active === 'cartDetails') {
     return (
@@ -31,34 +45,41 @@ function ContentCart({
     return (
       <div style={{ width: '100%' }}>
         <p>adresse de livraion: {user.adress}</p>
-        <p>Total panier: {totalCart}</p>
+        <p>Total panier: {total}</p>
         <p>Total produits: {numberOfproducts}</p>
       </div>
     );
   }
 
   return (
-    <div style={{ width: '100%' }}>
-      <button>achéter</button>
+    <div style={{ width: '100%', textAlign: 'center' }}>
+      <p>Si vous voulez commander cette pannier cliquer sur ACHETER</p>
+      <Button color="green" onClick={() => createOrder()}>ACHETER</Button>
     </div>
   );
 }
 
 ContentCart.propTypes = {
+  history: PropTypes.object,
   data: PropTypes.object,
   user: PropTypes.object,
   cart: PropTypes.object,
   active: PropTypes.string,
+  createOrderMutation: PropTypes.func,
 };
 
-export default graphql(getItemsCurrentCart, {
-  options: ({ cart }) => {
-    const products = get(cart, 'currentCart.cart.items', []);
-    const ids = map(products, (product) => product.productId);
-    return {
-      variables: {
-        ids,
-      },
-    };
-  },
-})(ContentCart);
+export default compose(
+  withRouter,
+  graphql(getItemsCurrentCart, {
+    options: ({ cart }) => {
+      const products = get(cart, 'currentCart.cart.items', []);
+      const ids = map(products, (product) => product.productId);
+      return {
+        variables: {
+          ids,
+        },
+      };
+    },
+  }),
+  withCreateOrderMutation
+)(ContentCart);
