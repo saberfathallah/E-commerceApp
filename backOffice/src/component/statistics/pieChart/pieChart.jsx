174a05fb0a3b35cce3901deb888dd2cc7faf04db
saber <saberfathallah7@gmail.com>
2019-06-11 11:58:36 +0200

    commit code chart

--- backOffice/src/component/statistics/pieChart/pieChart.jsx
+++ backOffice/src/component/statistics/pieChart/pieChart.jsx
@@ -2,53 +2,54 @@ import React from 'react';
 import { graphql } from 'react-apollo';
 import { get } from 'lodash';
 import PropTypes from 'prop-types';
-import Chart from 'react-google-charts';
+// import Chart from 'react-google-charts';
 import TOPUSERCOMMANDERS from '../../../graphql/statistics/topUserCommandesQuery';
 import getOrderStatistics from '../../../utils/getOrderStatistics';
 
-const pieOptions = {
-  title: '',
-  pieHole: 0.6,
-  slices: [
-    {
-      color: '#2BB673',
-    },
-    {
-      color: '#d91e48',
-    },
-    {
-      color: '#007fad',
-    },
-    {
-      color: '#e9a227',
-    },
-  ],
-  legend: {
-    position: 'bottom',
-    alignment: 'center',
-    textStyle: {
-      color: '233238',
-      fontSize: 14,
-    },
-  },
-  tooltip: {
-    showColorCode: true,
-  },
-  chartArea: {
-    left: 0,
-    top: 0,
-    width: '100%',
-    height: '80%',
-  },
-  fontName: 'Roboto',
-};
+// const pieOptions = {
+//   title: '',
+//   pieHole: 0.6,
+//   slices: [
+//     {
+//       color: '#2BB673',
+//     },
+//     {
+//       color: '#d91e48',
+//     },
+//     {
+//       color: '#007fad',
+//     },
+//     {
+//       color: '#e9a227',
+//     },
+//   ],
+//   legend: {
+//     position: 'bottom',
+//     alignment: 'center',
+//     textStyle: {
+//       color: '233238',
+//       fontSize: 14,
+//     },
+//   },
+//   tooltip: {
+//     showColorCode: true,
+//   },
+//   chartArea: {
+//     left: 0,
+//     top: 0,
+//     width: '100%',
+//     height: '80%',
+//   },
+//   fontName: 'Roboto',
+// };
 function PieChart({ data }) {
   const result = get(data, 'topUserCommandes', []);
   const statOrders = getOrderStatistics(result);
+  console.log(statOrders);
   return (
     <div className="App">
       <p>les plus fidéles utilisateurs</p>
-      <Chart
+      {/* <Chart
         chartType="PieChart"
         data={statOrders}
         options={pieOptions}
@@ -56,7 +57,7 @@ function PieChart({ data }) {
         width="100%"
         height="400px"
         legend_toggle
-      />
+      /> */}
     </div>
   );
 }
